b535d36f62e1b86256dd0d96e18e22c681f46ca4
leander <leander97@gmx.de>
2022-06-15 22:06:28 -0400

    added annotations in the public forward method; more abstract cpu/cuda handling in python module

--- capped_mean/capped_mean.py
+++ capped_mean/capped_mean.py
@@ -1,26 +1,29 @@
 import torch
+from torch import Tensor
+from torch.types import _bool
 
 import capped_mean_cpu, capped_mean_cuda
 
 class CappedMeanFunction(torch.autograd.Function) :
+
+    @staticmethod
+    def _dispatcher (function_name, x, *args) :
+        # small helper to abstract away the device
+        m = capped_mean_cuda if x.is_cuda else capped_mean_cpu
+        f = getattr(m, function_name)
+        return f(x, *args)
     
     @staticmethod
-    def forward (ctx, x, N, keepdim=False) :
+    def forward (ctx, x, N, keepdim) :
         ctx.save_for_backward(x, N)
-        if x.is_cuda :
-            return capped_mean_cuda.forward(x, N, keepdim)
-        else :
-            return capped_mean_cpu.forward(x, N, keepdim)
+        return CappedMeanFunction._dispatcher('forward', x, N, keepdim)
 
     @staticmethod
     def backward (ctx, grad) :
         x, N = ctx.saved_tensors
-        if grad.is_cuda :
-            xgrad = capped_mean_cuda.backward(x, N, grad)
-        else :
-            xgrad = capped_mean_cpu.backward(x, N, grad)
+        xgrad = CappedMeanFunction._dispatcher('backward', x, N, grad)
         
-        # obviously, output is not differentiable w.r.t N and keepdim
+        # obviously, output is not differentiable w.r.t. N and keepdim
         return xgrad, None, None
 
 
@@ -29,5 +32,5 @@ class CappedMean(torch.nn.Module) :
     def __init__(self) :
         super().__init__()
 
-    def forward(self, x, N, keepdim=False) :
+    def forward(self, x: Tensor, N: Tensor, keepdim: _bool=False) -> Tensor :
         return CappedMeanFunction.apply(x, N, False)
